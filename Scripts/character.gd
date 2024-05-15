1f7bd2ed9995309b7f8286a4389d7fc39d07520c
Manuel Espinoza <mespinoza@heuristyc.com>
2024-05-15 18:25:56 -0400

    adding movements animations

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,19 +1,26 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+@onready var animated_sprite = $AnimatedSprite2D
+
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if input_direction.x > 0:
+		animated_sprite.flip_h = false
+		animated_sprite.play("move_x")
+	elif input_direction.x < 0:
+		animated_sprite.flip_h = true
+		animated_sprite.play("move_x")
+	elif input_direction.y < 0:
+		animated_sprite.play("move_up")
+	elif input_direction.y > 0:
+		animated_sprite.play("move_down")
+	else:
+		animated_sprite.play("idle")
+		
+	velocity = input_direction * SPEED
 
 func _physics_process(delta):
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var directionX = Input.get_axis("move_left", "move_right")
-	var directionY = Input.get_axis("move_up", "move_down")
-	if directionX:
-		velocity.x = directionX * SPEED
-	elif directionY:
-		velocity.y = directionY * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	get_input()
 	move_and_slide()
