833abe6e58cb4f9a6b9bf0776ba7b5c9ae272498
Omar Rojas Rubino <orojas@heuristyc.com>
2024-05-17 17:11:14 -0300

    Enemies Demage

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -11,14 +11,11 @@ func _physics_process(delta):
 	target_position = (character.position - position).normalized()
 	move_and_collide(target_position * 2)
 
-
-func _on_hurt_box_area_area_entered(area):
-	if area.is_in_group("Sword"):
-		dead = true
-		animated_sprite.play("dead")
-		
-
-
 func _on_animated_sprite_2d_animation_finished():
 	if animated_sprite.animation == "dead":
 		queue_free()
+
+func _on_hurting_area_2d_area_entered(area):
+	if area.is_in_group("Sword"):
+		dead = true
+		animated_sprite.play("dead")
