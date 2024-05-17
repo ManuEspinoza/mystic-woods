0e34ab7f5c4e682c4c9aa6ab6149aa72b648c0d2
Manuel Espinoza <mespinoza@heuristyc.com>
2024-05-17 07:16:49 -0400

    cleaning player movement code

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,34 +1,47 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-var last_direction = "move_down"
+const MOVE_LEFT = "move_left"
+const MOVE_RIGHT = "move_right"
+const MOVE_UP = "move_up"
+const MOVE_DOWN = "move_down"
+const MOVE_SIDE = "move_side"
+const IDLE_SIDE = "idle_side"
+const IDLE_BACK = "idle_back"
+const IDLE = "idle"
+
+var direction = "MOVE_DOWN"
 @onready var animated_sprite = $AnimatedSprite2D
 
-func handle_input(move, flip = false):
+func movement_animation(move, flip = false):
 	animated_sprite.flip_h = flip
 	animated_sprite.play(move)
-	last_direction = move
-	
-func get_input():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if input_direction.x > 0:
-		handle_input("move_x")
-	elif input_direction.x < 0:
-		handle_input("move_x", true)
-	elif input_direction.y < 0:
-		handle_input("move_up")
-	elif input_direction.y > 0:
-		handle_input("move_down")
+	direction = move
+
+func idle_animation():
+	if direction == MOVE_UP:
+		animated_sprite.play(IDLE_BACK)
+	elif direction == MOVE_DOWN:
+		animated_sprite.play(IDLE)
 	else:
-		if last_direction == "move_up":
-			animated_sprite.play("idle_back")
-		elif last_direction == "move_down":
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("idle_x")
-		
+		animated_sprite.play(IDLE_SIDE)
+	
+func player_movement():
+	var input_direction = Input.get_vector(MOVE_LEFT, MOVE_RIGHT, MOVE_UP, MOVE_DOWN)
+	if input_direction.x > 0:
+		movement_animation(MOVE_SIDE)
+	elif input_direction.x < 0:
+		movement_animation(MOVE_SIDE, true)
+	elif input_direction.y < 0:
+		movement_animation(MOVE_UP)
+	elif input_direction.y > 0:
+		movement_animation(MOVE_DOWN)
+	else:
+		idle_animation()
+			
 	velocity = input_direction * SPEED
 
-func _physics_process(delta):
-	get_input()
+func _physics_process(delta):	
+	player_movement()
 	move_and_slide()
+	
