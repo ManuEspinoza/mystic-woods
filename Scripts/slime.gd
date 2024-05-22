e422244bb6933c618ee82313b4be5b43f0bae410
Omar Rojas Rubino <orojas@heuristyc.com>
2024-05-22 14:22:25 -0300

    Adding enemies spawns

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
-@onready var player = %Player
+@onready var player = get_node("/root/Game/Player")
 @onready var animated_sprite = $AnimatedSprite2D
 var target_position
-
 const SPEED = 200.0
 var dead = false
+
 func _physics_process(delta):
 	if dead == false:
 		animated_sprite.play("move")
---
+++ Scripts/spawner.gd
@@ -0,0 +1,25 @@
+extends Node2D
+const slime_scene := preload("res://Scenes/slime.tscn")
+@onready var timer = $Timer
+@onready var game = get_node("/root/Game")
+var spawn_points := []
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	populate_spawn_points()
+	timer.start()
+	spawn()
+	
+func populate_spawn_points():
+	for i in get_children():
+		if i is Marker2D:
+			spawn_points.append(i)
+			
+func spawn():
+	#pick random spawner
+	var spwan = spawn_points[randi() % spawn_points.size()]
+	var enemy = slime_scene.instantiate()
+	enemy.position = spwan.position
+	game.add_child(enemy)
+
+func _on_timer_timeout():
+	spawn()
