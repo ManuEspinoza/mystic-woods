28f3394dc695fa967c42afb8b9756ec085219a68
Manuel Espinoza <manuel.espinoza1996@gmail.com>
2024-05-25 10:50:00 -0400

    Adding drops

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
+
+@onready var game = get_node("/root/Game")
 @onready var player = get_node("/root/Game/Player")
 @onready var animated_sprite = $AnimatedSprite2D
-var target_position
+
 const SPEED = 200.0
+const PROBABILITY = 5
+
+var healer_item := preload("res://Scenes/healer.tscn")
+var target_position
 var dead = false
 var damage = 10
 
@@ -14,9 +20,20 @@ func _physics_process(delta):
 
 func _on_animated_sprite_2d_animation_finished():
 	if animated_sprite.animation == "dead":
+		drop_item()
 		queue_free()
 
 func _on_damage_area_area_entered(area):
 	if area.is_in_group("Sword"):
-		dead = true
-		animated_sprite.play("dead")
+		die()
+
+func die():
+	dead = true
+	animated_sprite.play("dead")
+
+func drop_item():
+	if (randi() % PROBABILITY) == (PROBABILITY - 1): 
+		var healer = healer_item.instantiate()
+		healer.position = position
+		game.call_deferred("add_child", healer)
+	
