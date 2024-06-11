3f673f5fe950bbf76e8d18409f4c63160724ccee
Omar Rojas Rubino <orojas@heuristyc.com>
2024-06-11 08:38:24 -0300

    adding shades to the shield and fixing visibility bugs

--- Scripts/droppable_shield.gd
+++ Scripts/droppable_shield.gd
@@ -1,6 +1,12 @@
 extends Droppable
 const SHIELD = preload("res://Scenes/shield.tscn")
 
-func effect(body: Player):
+func effect(player: Player):
+	stop_another_shield(player)
 	var shield = SHIELD.instantiate()
-	body.add_child(shield)
+	player.add_child(shield)
+	
+func stop_another_shield(player: Player):
+	for node in player.get_children():
+		if node is Shield:
+			node.queue_free()
--- Scripts/shield.gd
+++ Scripts/shield.gd
@@ -1,9 +1,9 @@
-extends Damager
+class_name Shield extends Damager
 @onready var player = get_node("/root/Game/Player")
+@onready var timer = $Timer
 
 func _ready():
 	position = to_local(player.position).normalized()
-
 
 func _on_timer_timeout():
 	queue_free()
