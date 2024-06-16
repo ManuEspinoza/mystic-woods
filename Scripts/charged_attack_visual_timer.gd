864c85e5040fb4a5d5556f4692d728bfe803fd68
Omar Rojas Rubino <orojas@heuristyc.com>
2024-06-16 19:43:50 -0300

    adding some stuff

---
+++ Scripts/charged_attack_visual_timer.gd
@@ -0,0 +1,13 @@
+extends Node2D
+@onready var progress_bar = $ProgressBar
+@onready var label = $Label
+#@export var timer: Timer
+
+#func _ready():
+	#label.text = timer.time_left
+	#progress_bar.max_value = timer.time_left
+	#progress_bar.value = timer.time_left
+
+#func _process(delta):
+	#label.text = timer.time_left
+	#progress_bar.value = timer.time_left
